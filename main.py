a812e60efbe311bc7dd209e6c6f29c4a61e1a280
otisnth <otisnth@gmail.com>
2023-11-20 20:31:41 +0300

    добавлен шаблон бота

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 import os
 
-print("Secret value:", os.environ.get("TOKEN"))
+token = os.environ.get("TOKEN")
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(token).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
